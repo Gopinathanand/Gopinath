3330bd70beb54806c9aa447d509960aa3fe4d965
Gopinath <gnath8940@gmail.com>
2017-12-09 21:23:05 +0530

    sample

---
+++ app/src/test/java/com/example/gopin/dice/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.example.gopin.dice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gopin on 09-12-2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate1() throws Exception {
+    }
+
+    @Test
+    public void randomDiceValue1() throws Exception {
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void randomDiceValue() throws Exception {
+    }
+
+}
